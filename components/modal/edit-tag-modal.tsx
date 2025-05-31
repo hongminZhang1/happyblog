634072295cb9e08a2060341c042a6ee2fae3c2e8
Ye Yu <nearjilt@gmail.com>
2025-05-31 13:41:28 +0800

    feat(tag): 将edit-tag-modal中的store移出,使用tanstack-query替代

--- components/modal/edit-tag-modal.tsx
+++ components/modal/edit-tag-modal.tsx
@@ -2,7 +2,6 @@
 
 import type { UpdateTagNameDTO } from '@/actions/tags/type'
 import {
-  getAllTags,
   updateBlogTagById,
   updateNoteTagById,
 } from '@/actions/tags'
@@ -25,9 +24,9 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { useModalStore } from '@/store/use-modal-store'
-import { useTagStore } from '@/store/use-tag-store'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { TagType } from '@prisma/client'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
@@ -35,67 +34,47 @@ import { toast } from 'sonner'
 export default function EditTagModal() {
   const { modalType, onModalClose, payload } = useModalStore()
   const isModalOpen = modalType === 'editTagModal'
-  const { setTags } = useTagStore()
   const { id, tagName, tagType } = payload
     ? (payload as UpdateTagNameDTO)
     : {}
 
-  const form = useForm<UpdateTagNameDTO>({
-    resolver: zodResolver(UpdateTagNameSchema),
-    defaultValues: {
-      id: id ?? -1,
-      tagName: tagName ?? '',
-      tagType: tagType ?? TagType.BLOG,
-    },
-    mode: 'onBlur',
-  })
-
-  useEffect(() => {
-    if (isModalOpen && tagName) {
-      form.reset({
-        id: id ?? -1,
-        tagName: tagName ?? '',
-        tagType: tagType ?? TagType.BLOG,
-      })
-    }
-  }, [isModalOpen, form, tagName, id, tagType])
-
-  const handleTagNameChange = async (values: UpdateTagNameDTO) => {
-    try {
-      switch (tagType) {
-        case TagType.BLOG:
-          await updateBlogTagById(values)
-          break
-        case TagType.NOTE:
-          await updateNoteTagById(values)
-          break
-        default:
-          throw new Error('标签类型错误!')
-      }
-
-      const allTags = await getAllTags()
-      setTags(allTags)
-
+  const queryClient = useQueryClient()
+  const { mutate, isPending } = useMutation({
+    mutationFn: updateTagName,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tags'] })
       toast.success(`修改成功`)
-    }
-    catch (error) {
+    },
+    onError: (error) => {
       if (error instanceof Error) {
         toast.error(`修改标签出错 ${error.message}`)
       }
       else {
         toast.error(`修改标签出错`)
       }
+    },
+  })
+
+  const form = useForm<UpdateTagNameDTO>({
+    resolver: zodResolver(UpdateTagNameSchema),
+    mode: 'onBlur',
+  })
+
+  useEffect(() => {
+    if (isModalOpen && tagName) {
+      form.reset({
+        id: id!,
+        tagName: tagName!,
+        tagType: tagType!,
+      })
     }
-  }
+  }, [isModalOpen, form, tagName, id, tagType])
 
   async function onSubmit(values: UpdateTagNameDTO) {
-    if (!id || id === -1 || !tagName) {
-      toast.error(`id 和标签名为空`)
-      return
-    }
-    await handleTagNameChange({ ...values })
+    mutate(values)
     onModalClose()
   }
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onModalClose}>
       <DialogContent className="sm:max-w-[425px]">
@@ -121,11 +100,24 @@ export default function EditTagModal() {
                   </FormItem>
                 )}
               />
-              <Button type="submit" className="cursor-pointer">保存修改</Button>
+              <Button type="submit" className="cursor-pointer" disabled={isPending}>保存修改</Button>
             </form>
           </Form>
         </div>
       </DialogContent>
     </Dialog>
   )
+}
+
+async function updateTagName(values: UpdateTagNameDTO) {
+  switch (values.tagType) {
+    case TagType.BLOG:
+      await updateBlogTagById(values)
+      break
+    case TagType.NOTE:
+      await updateNoteTagById(values)
+      break
+    default:
+      throw new Error('标签类型错误!')
+  }
 }
