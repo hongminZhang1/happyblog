7f3f4ad54b311085e635731d85e64a349025f8ea
Ye Yu <nearjilt@gmail.com>
2025-06-02 17:45:17 +0800

    feat(note): 使用tanstack-query替代store

--- modules/admin/page/admin-note-page/internal/publish-toggle-switch.tsx
+++ modules/admin/page/admin-note-page/internal/publish-toggle-switch.tsx
@@ -1,35 +1,35 @@
 import { toggleNotePublishedById } from '@/actions/notes'
 import { Switch } from '@/components/ui/switch'
-import { useNoteStore } from '@/store/use-note-store'
-import { useTransition } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import { useState, useTransition } from 'react'
 import { toast } from 'sonner'
 
 export default function PublishToggleSwitch({
   noteId,
-  isPublished,
+  isPublished: initial,
 }: {
   noteId: number
   isPublished: boolean
 }) {
-  const { notes, setNotes } = useNoteStore()
+  const [isPublished, setIsPublished] = useState(initial)
   const [isPending, startTransition] = useTransition()
+  const queryClient = useQueryClient()
 
   const handleToggle = async () => {
     const newStatus = !isPublished
-    const preNotes = [...notes]
-
-    const updated = notes.map(item =>
-      item.id === noteId ? { ...item, isPublished: newStatus } : item,
-    )
-    setNotes(updated)
+    setIsPublished(newStatus)
 
     startTransition(async () => {
       try {
         await toggleNotePublishedById(noteId, newStatus)
+        queryClient.invalidateQueries({
+          queryKey: ['note-list'],
+          exact: false,
+        })
         toast.success(`更新成功`)
       }
       catch (error) {
-        setNotes(preNotes)
+        setIsPublished(!newStatus)
         if (error instanceof Error) {
           toast.error(`发布状态更新失败 ${error?.message}`)
         }
